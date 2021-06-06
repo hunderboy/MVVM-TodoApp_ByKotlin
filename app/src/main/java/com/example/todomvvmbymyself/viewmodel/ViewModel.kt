ab45f3de64744a413e3e9abb0657032378fe026c
Dev_dominos <masury2011@gmail.com>
2021-06-07 03:04:16 +0900

    // 뷰모델 작성중

---
+++ app/src/main/java/com/example/todomvvmbymyself/viewmodel/ViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.todomvvmbymyself.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class ViewModel(application: Application) : AndroidViewModel(application) {
+
+}
